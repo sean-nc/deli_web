5837911c48959e4a41b8b13b9984f1c89b969dbd
Sean <sean.n.cordes@gmail.com>
2018-08-30 17:08:30 -0400

    initial setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  private
+  def is_user?
+    unless user_signed_in?
+      flash[:notice] = "Please sign in first."
+      redirect_to new_user_session_path
+    end
+  end
 end
