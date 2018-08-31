61bd4a339b0be9b2c7e95435cc62998113ea8e4a
Sean <sean.n.cordes@gmail.com>
2018-08-31 00:26:52 -0400

    added order functionality for controller and views

--- app/controllers/cheese_orders_controller.rb
+++ app/controllers/cheese_orders_controller.rb
@@ -2,16 +2,35 @@ class CheeseOrdersController < ApplicationController
   before_action :is_user?
 
   def index
+    cheese = Cheese.friendly.find(params[:id])
+    @orders = cheese.orders
   end
 
   def new
+    cheese = Cheese.friendly.find(params[:id])
+    @order = cheese.orders.build
   end
 
   def create
+    cheese = Cheese.friendly.find(params[:id])
+    @order = cheese.orders.build(cheese_order_params)
+    if @order.save
+      redirect_to @order.cheese
+      flash[:notice] = 'Order was successfully created.'
+    else
+      render :new
+    end
   end
 
   def destroy
-    @order = Order.find(params[:id])
+    @order = CheeseOrder.find(params[:id])
     @order.destroy
+    redirect_to @order.cheese
+    flash[:notice] = 'Order was successfully destroyed.'
   end
+
+  private
+    def cheese_order_params
+      params.require(:cheese_order).permit(:cheese_id, :current_units, :ordered_units)
+    end
 end
--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -2,7 +2,7 @@ class Cheese < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
   has_one_attached :image
-  has_many :orders, class_name: "CheeseOrders"
+  has_many :orders, class_name: "CheeseOrder"
   belongs_to :supplier
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :image, presence: true
